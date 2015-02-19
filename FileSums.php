bd0437386b26ac3f96c3ab8fddfc72dbdfc6b511
Jeremy P <jrahmy+github@gmail.com>
2015-02-18 23:28:33 -0800

    Update release files

--- FileSums.php
+++ FileSums.php
@@ -26,10 +26,10 @@ class FileSums
     public static function getHashes()
     {
         return [
-                'library/Jrahmy/PollsList/ControllerPublic/Polls.php' => 'a9695e5d1587fc428165d7a038c8b5a5',
+                'library/Jrahmy/PollsList/ControllerPublic/Polls.php' => 'e97698f8b97982c87a35ca2f48b42d34',
                 'library/Jrahmy/PollsList/Listener.php' => '8625687a252220d5d33b0d4f55c321d0',
-                'library/Jrahmy/PollsList/Model/Poll.php' => '3cd4eb732b1c0833b024a5414bbd847c',
                 'library/Jrahmy/PollsList/Route/Prefix/Polls.php' => '2f92ee1b34e42ca0543ee6f2fe928afd',
+                'library/Jrahmy/PollsList/Model/Poll.php' => 'f642073ba5e6f5a148af2befaa6f724a',
 
         ];
     }
